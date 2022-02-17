462efcaf7ec8134093f56725925a096df23ac76a
Roman <bilkasnikers@gmail.com>
2022-02-17 20:08:42 +0200

    add js

---
+++ js/script.js
@@ -0,0 +1,147 @@
+// let a = 5, b = 4;
+//     console.log(a)
+//     console.log(b)
+//     let sum = a + b
+//     console.log(sum)
+
+
+
+    // const
+    // const n = 10
+    // console.log(n)
+    // let a = n
+    // console.log(a)
+    // n = 120 // error
+// task 1
+    // let r = 5
+    // const PI = 3.14
+
+    // let S = PI * r * r
+    // console.log(S)
+    // ler P = 
+    
+
+    // типи данних
+
+// let a = 10
+// let b = 34.245
+// typeof b
+// typeof a
+// console.log(typeof a)
+// console.log(typeof b)
+
+// string
+
+// let login = "user1"
+
+// console.log(typeof login)
+
+
+
+// boolean (true,false)
+
+// let b = true
+// console.log(typeof b)
+
+
+// underfined
+
+// let a
+// console.log(typeof a)
+
+
+// Оператор, Операція, Операнд
+
+// let a = 3, b = 1
+// let res = a - b
+
+//в залежності від кількості операндів оператори поділяються на 1 :
+// унарні ( +, -, ++, --)
+// бінарні ( +, -, /, *, =, ==, !=, >, <, %)
+// тернарний 
+
+// let a = 5
+// let b = -a
+
+// console.log(a)
+// console.log(b)
+
+
+// ++ інкремент (збільшує на одиницю)
+// -- декремент (зменшує на одиницю)
+
+// мають 2 форми:
+// префіксна форма
+// постфіксна
+
+// let a = 3
+// a++
+// console.log(a)
+
+// let a = 4
+// console.log(a)
+
+
+// ділення по модулю (оператор обчислення залишку від цілочисельного ділення)
+
+// let a = 5
+// let b = a % 2
+// console.log(b)
+
+
+// let num = 34
+// let a , b
+
+// a = Math.trunc(num / 10)
+// b = num % 10
+
+// console.log(a)
+// console.log(b)
+
+
+// let num = 456
+
+// let a, b, c
+
+// a = Math.trunc(num / 100)
+// console.log(a)
+
+// b = Math.trunc(num / 10) % 10
+// console.log(b)
+
+
+
+// c = num % 10
+// console.log(c)
+
+// let num = 1234
+// a = Math.trunc(num / 1000) % 100
+
+// console.log(a)
+
+// b = Math.trunc(num / 100) % 10
+// console.log(b)
+
+// c = Math.trunc(num / 10) % 10
+// console.log(c)
+
+// d = num % 10
+// console.log(d)
+
+
+let s = 150
+const min = 60
+
+let clock = s / min
+d = Math.trunc(clock)
+console.log(d)
+
+let sek = s % min
+console.log(sek)
+
+
+
+
+
+
+
