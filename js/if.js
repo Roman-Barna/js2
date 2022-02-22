d64d9c6ef269f4ce48f52ae90976690c09975bac
Roman <bilkasnikers@gmail.com>
2022-02-22 20:55:51 +0200

    add js33

---
+++ js/if.js
@@ -0,0 +1,180 @@
+// оператор логічного вибору if
+/* 
+ Загальний синтаксис оператора if
+
+ if(умова) {
+    блок 1
+}
+else {
+    блок 2
+}
+*/
+
+
+// let a = +prompt("Enter num1: ")
+// let b = +prompt("Enter num2: ")
+
+// if (a > b) {
+//     console.log(a)
+// }
+// else {
+//     console.log(b)
+// }
+
+// let a = +prompt("Enter num: ")
+
+// if (a >= 12) {
+//     alert("до обіду")
+// }else{
+//     alert("після обіду")
+// }
+
+
+// let num1 = +prompt("Enter Score 1-12: "),
+// num2 = +prompt("Enter Score 1-12: "),
+// num3 = +prompt("Enter Score 1-12: "),
+// num4 = +prompt("Enter Score 1-12: "),
+// num5 = +prompt("Enter Score 1-12 ")
+
+// let res = (num1 + num2 + num3 + num4 + num5 ) / 5
+
+
+// if (res >= 7) {
+//     alert('Passed')
+// }else{
+//     alert('No passed') 
+// }
+
+
+
+// let a = 5
+// if(a > 2) (
+//     alert('hellow')
+// )
+
+
+//
+
+// let num = +prompt('add number')
+
+// if(num > 0){
+//     alert('це додатнє число')
+// }
+// if(num < 0){
+//     alert('це відємне число')
+// }
+// if(num == 0){
+//     alert('це 0')
+// }
+
+//
+
+// if(num > 0) {
+//     alert("+")
+// }
+// else {
+//     if(num < 0) {
+//         alert("-")
+//     }
+//     else {
+//         alert("Нуль")
+//     }
+// }
+
+
+//
+
+// if(num > 0) 
+//     alert("+")
+// else if(num < 0)
+//     alert("-")
+// else 
+//     alert("нуль")
+
+
+// let a = +prompt('add number')
+// let b = +prompt('add number')
+// let c = +prompt('add number')
+
+// if(a > b && a > c) {
+//     alert(a)
+// }
+// else {
+//     if(b > c) {
+//         alert(b)
+//     }
+//     else {
+//         alert(c)
+//     }
+// }
+
+// if(a > b) {
+//     if(a > c) {
+//         alert(a)
+//     }
+//     else {
+//         alert(c)
+//     }
+// }
+// else {
+//     if(b > c) {
+//         alert(b)
+//     }
+//     else {
+//         alert(c)
+//     }
+// }
+
+
+// let num = +prompt('add number')
+// if (num % 2 == 0){
+//     alert("парне")
+// }
+// else {
+//     alert("не парне")
+// }
+
+// let num = +prompt('add number')
+
+// if(num >= 0 && num <= 100) {
+//     alert('належить до діапазону')
+// }
+// else {
+//       alert("не належить до діапазону")
+//   }
+
+//  let min = +prompt('веедіть нижню границю')
+//  let max = +prompt('веедіть верхню границю')
+//  let num = +prompt('веедіть число')
+
+//  if(num >= min && num <= max) {
+//      alert('належить до діапазону')
+//  }
+//  else {
+//        alert("не належить до діапазону")
+//    }
+
+//  let num1 = +prompt('Enter number1')
+//  let num2 = +prompt('Enter number2')
+//  let num3 = +prompt('Enter number3')
+
+// let a = 1, b = 2, c = 3
+
+// let a = +prompt("1")
+// let b = +prompt("2")
+// let c = +prompt("3")
+// let sum 
+
+// if(a > b && c > b) {
+//     sum = a + c
+// }
+
+// if(a > c && b > c) {
+//     sum = a + b
+// }
+
+// if(c > a && b > a) {
+//     sum = b + c
+// }
+
+// alert(sum)
---
+++ js/script.js
@@ -0,0 +1,71 @@
+// let a = +prompt('add grn')
+// let b = +prompt('add Euro')
+// let c = +prompt('add zl')
+// let $ = +prompt('add curs $')
+// let Euro = +prompt('add curs Euro')
+// let zl = +prompt('add curs  zl')
+// let $1 = a / $
+// let Euro1 = a / Euro
+// let zl1 = a / zl
+// alert('$ =' + $1 + ',' + 'Euro =' + Euro1 + ',' + 'zl =' + zl1)
+
+
+// Логічні оператори завжди:  (true або  false)
+//  оператори порівняння
+// === - ІДЕНТИЧНІСТЬ
+// == - дорівнює
+// != - не дорівнює
+// > - більше 
+// < - менше
+// <= - менше рівне
+// >= - більше рівне
+// && - логічне і
+// || - логічне або
+// ! - логічне не(заперечиння)
+
+// let a = 5, b = 5 
+
+// console.log(a == b)
+//  true && false 
+// let a = 3, b = 4, c = 5
+// console.log(a < b && b > c)
+// let a = 3, b = 4
+// let max = a > b ? a : b
+// console.log(max)
+
+//  let a = 3, b = 4, c = 5
+
+// let max = a > b ? (a > c ? a : c) : (b > c ? b : c)
+// console.log(max)
+
+// let a = 3, b = 4, c = 5
+// let max = a > b && a > c ? a : b > c ? b : c
+// console.log(max)
+
+// let num = +prompt('enter a lucky 3-digit number')
+// let a = Math.trunc(num / 100),
+// b = Math.trunc(num / 10) % 10,
+// c = num % 10
+
+// a == b && a == c ?
+//  alert('lucky number') : alert('not a lucky number')
+
+
+
+// ** - оператор піднесення до степені
+// let a = 5 
+// let res = a ** 3
+// console.log(res)
+
+
+// Скорочені форми оператора присвоєння:
+// +=, -=, *=, /=, %=
+
+// let a = 10, b = 5
+// a = a + b       
+// a += b
+
+// console.log(a)
+// let a = 5, b = 4
+// console.log(!(a > b))
+
